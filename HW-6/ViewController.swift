8f6bf8f8ae1e1cc03e64bf3e7dd91b452820a5e6
ramil95 <rgadjiev95uzao@gmail.com>
2022-03-07 11:34:04 +0300

    Initial Commit

---
+++ HW-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-6
+//
+//  Created by Рамил Гаджиев on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
